1cc46feda39c3b8d9479052b284bd8c85c79126e
DaffaPutra7 <putradika3604@gmail.com>
2024-10-17 15:56:51 +0700

    Menambahkan gambar, disini saya menambahkan logo umy dengan nama umy dan ditambahkan pada package drawable di file resource dan saya memanggilnya dengan menggunakan komponen Image setelah itu mengisi id yang berisi R.drawable.umy(nama file image) yang terakhir menambahkan contentDescription yang disetting ke null jika tidak ditambahkan teks

--- app/src/main/java/com/example/questbasiccomposable_133/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_133/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.questbasiccomposable_133
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,6 +53,10 @@ fun BasicLayout(modifier: Modifier = Modifier) {
         )
 
         Text(text = "Ini adalah halaman Login", style = TextStyle(fontSize = 20.sp))
+        Image(painter = painterResource(id = R.drawable.umy),
+            contentDescription = null,
+
+        )
     }
 }
 
