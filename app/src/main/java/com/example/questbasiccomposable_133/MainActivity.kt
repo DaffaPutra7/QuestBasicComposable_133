ec78568a56250081aac3130fa5a9ef13db634a36
DaffaPutra7 <putradika3604@gmail.com>
2024-10-17 15:21:38 +0700

    Membuat class BasicLayout di MainActivity dan memasukkan class BasicLayout di class MainActivity menggantikan class Greeting. Dan ini juga dilakukan pada bagian @Preview Composable

---
+++ app/src/main/java/com/example/questbasiccomposable_133/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.questbasiccomposable_133
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questbasiccomposable_133.ui.theme.QuestBasicComposable_133Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestBasicComposable_133Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicLayout(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicLayout(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestBasicComposable_133Theme {
+        BasicLayout()
+    }
+}
