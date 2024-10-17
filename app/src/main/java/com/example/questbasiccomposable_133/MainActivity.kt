314ef5dee0d54639a928abc3213bd1af5ccf18ca
DaffaPutra7 <putradika3604@gmail.com>
2024-10-17 15:30:06 +0700

    Membuat kolom menggunakan elemen Colum dan diisi dengan beberapa pengaturan seperti mengatur agar layout yang dihasilkan sejajar secara horizontal di tengah, mengatur agar layout diatur dari atas dan mengatur agar kolom memenuhi seluruh ukuran layar

--- app/src/main/java/com/example/questbasiccomposable_133/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_133/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_133.ui.theme.QuestBasicComposable_133Theme
@@ -31,7 +34,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicLayout(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize()
+    ) {
 
+    }
 }
 
 @Preview(showBackground = true)
